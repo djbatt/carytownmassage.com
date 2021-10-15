228d5e769db5d8cb67f444a2c852494dce158399
djbatt <itsalldave@gmail.com>
2021-10-15 14:35:05 -0400

    First Commit and Bootstrapping
    Update ESLint to allow display=block fonts and remove error message for
    custom _document.
    
    Added google tag, global styles, theming.
    
    Added head via next-seo and added sitemap
    
    Basic Security Headers Added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,36 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Content-Security-Policy",
+    value:
+      " default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob: 'unsafe-inline'; frame-src *; style-src * 'unsafe-inline'; ",
+  },
+];
+
 module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
