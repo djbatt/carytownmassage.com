13153a07fbd7965d88413dad0f547b42bda4c4c9
djbatt <itsalldave@gmail.com>
2021-10-15 14:39:28 -0400

    added default seo config

---
+++ next-seo.config.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Massage On Main - Massage Richmond, VA",
+  description:
+    "Massage On Main is located in the heart of Richmond, VA. Massage will help you escape from today's erratic and busy life. Massage Therapy Richmond VA.",
+  additionalLinkTags: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+    },
+  ],
+};
