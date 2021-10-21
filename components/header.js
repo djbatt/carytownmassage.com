3b965fed3f53a2256f6afb6a389e551285b758b7
djbatt <itsalldave@gmail.com>
2021-10-21 14:04:51 -0400

    Finished header
    Header is finished and responds responsively to changes in viewport
    Added mobile button for booking and added hamburger navigation menu

--- components/header.js
+++ components/header.js
@@ -1,65 +1,94 @@
 import Link from "next/link";
 import Image from "next/image";
 import MassageOnMainLogo from "../public/Massage-On-Main-Logo.svg";
-import { forwardRef } from "react";
+import { forwardRef, useState, useEffect } from "react";
+import { Squash as Hamburger } from "hamburger-react";
+
+//Styles
 import styles from "../styles/header.module.scss";
 
 export default function Header(props) {
+  //Logo Anchor forwardRef
   const Logo = forwardRef(({ onClick, href, children }, ref) => {
     return (
-      <a
-        href={href}
-        onClick={onClick}
-        ref={ref}
-        style={{
-          width: "fit-content",
-          height: "fit-content",
-          margin: 0,
-          display: "flex",
-          flexDirection: "column",
-          flex: 1,
-        }}
-      >
-        <div className={styles.logoContainer}>
-          <Image
-            src={MassageOnMainLogo}
-            layout="responsive"
-            height="100"
-            width="291"
-            priority
-          ></Image>
-        </div>
+      <a href={href} onClick={onClick} ref={ref} className={styles.logoAnchor}>
+        <Image
+          src={MassageOnMainLogo}
+          layout="responsive"
+          width={291}
+          height={100}
+          priority
+        ></Image>
       </a>
     );
   });
+
+  //Hamburger State
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <Link href="/" passHref>
-          <Logo />
-        </Link>
-        <ul className={styles.navList}>
-          <li className={styles.navListItem}>
-            <Link href="/">Home</Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/">About</Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/">Services</Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/">
-              <a className={styles.phoneNumber}>(804) 424-2607</a>
-            </Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/">
-              <a className={styles.booking}>Booking</a>
-            </Link>
-          </li>
-        </ul>
+        <div className={styles.logoDiv}>
+          <Link href="/" passHref>
+            <Logo />
+          </Link>
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            label="Navigation Menu"
+            color="#158ba8"
+          />
+        </div>
+        <div className={styles.collapsingDiv} style={isOpen ? {display: "flex"} : {display: "none"}}>
+          <ul className={styles.navList}>
+            <li className={styles.navListItem}>
+              <Link href="/">Home</Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link href="/">About</Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link href="/">Services</Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link href="/">
+                <a className={styles.phoneNumber}>(804) 424-2607</a>
+              </Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link
+                href="https://square.site/book/LH466QXPEW2KV/massage-on-main-llc-richmond-va"
+                passHref
+              >
+                <a className={styles.booking}>Booking</a>
+              </Link>
+            </li>
+          </ul>
+          <ul className={styles.contactList}>
+            <li className={styles.contactListItem}>
+              <div className={styles.spacer}></div>
+            </li>
+            <li className={styles.contactListItem}>
+              <span>Anthony@massageonmainrva.com</span>
+            </li>
+            <li className={styles.contactListItem}>
+              <span>2602 West Main St.</span>
+            </li>
+            <li className={styles.contactListItem}>
+              <span>Richmond, Virginia 23220</span>
+            </li>
+          </ul>
+        </div>
       </nav>
+      <div className={styles.mobileBookingDiv}>
+        <Link
+          href="https://square.site/book/LH466QXPEW2KV/massage-on-main-llc-richmond-va"
+          passHref
+        >
+          <a className={styles.mobileBookingAnchor}>Booking</a>
+        </Link>
+      </div>
     </header>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Header from "../components/header";
+import styles from "../styles/index.module.scss";
+import Image from "next/image";
+
+import HeroImage from "../public/photos/hans-vivek-UiMkBvDQSAA-unsplash.jpg";
 
 export default function Home() {
-  
   return (
     <>
       <Header />
-      <main style={{backgroundColor: "grey"}}></main>
+      <main>
+        <div className={styles.heroImage}>
+          {/* <Image src={HeroImage} layout="fill" objectFit="cover"></Image> */}
+        </div>
+      </main>
       <footer></footer>
     </>
   );
